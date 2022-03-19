26662b33e4f05aad15e9ae2a7093fc60461c9728
HoshinoSuzumi <boxlab@foxmail.com>
2022-03-20 02:12:05 +0800

    fix: FormUrlEncoded

--- src/main/java/moe/ibox/tsapi/service/ServiceManagement.java
+++ src/main/java/moe/ibox/tsapi/service/ServiceManagement.java
@@ -5,9 +5,16 @@ import moe.ibox.tsapi.entities.response.ServiceCreated;
 import moe.ibox.tsapi.entities.response.base.BaseResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface ServiceManagement {
+    @FormUrlEncoded
     @POST("/service/add")
-    Call<BaseResponse<ServiceCreated>> addService(@Body ServiceAdd serviceAdd);
+    Call<BaseResponse<ServiceCreated>> addService(
+            @Field("key") String key,
+            @Field("name") String name,
+            @Field("desc") String description);
+//    Call<BaseResponse<ServiceCreated>> addService(@Body ServiceAdd serviceAdd);
 }
